f1cc8e3e57facf7c2bedb1cac6cbf230466c561e
nrslib <masanobu.naruse@gmail.com>
2022-03-06 16:55:56 +0900

    avoid using import

--- src/base/_02/main.py
+++ src/base/_02/main.py
@@ -1,1 +1,8 @@
+# JankenGame クラスを定義しよう
+
 # JankenGame クラスを使ってジャンケンをしよう
+def main():
+    return
+
+if __name__ == '__main__':
+    main()
--- src/sample/_04/main.py
+++ src/sample/_04/main.py
@@ -1,4 +1,61 @@
-from janken import JankenGame
+class JankenGame:
+    def play(self, left_hand: int, right_hand: int, lang: str):
+        """
+        :param lang: ja:日本語,en:英語
+        """
+        result = self.judge(left_hand, right_hand)
+        self.show_result(result, lang)
+
+    def judge(self, left_hand: int, right_hand: int) -> int:
+        if left_hand == 0:  # Goo
+            if right_hand == 0:  # Goo
+                return 0
+            elif right_hand == 1:  # Choki
+                return 1
+            else:  # Par
+                return -1
+        elif left_hand == 1:  # Choki
+            if right_hand == 0:  # Goo
+                return -1
+            elif right_hand == 1:  # Choki
+                return 0
+            else:  # Par
+                return 1
+        else:  # Par
+            if right_hand == 0:  # Goo
+                return 1
+            elif right_hand == 1:  # Choki
+                return -1
+            else:  # Par
+                return 0
+
+    def show_result(self, result: int, lang: str):
+        if lang == "ja":
+            display = JapaneseDisplay()
+            display.show(result)
+        else:
+            display = EnglishDisplay()
+            display.show(result)
+
+
+class JapaneseDisplay:
+    def show(self, result: int):
+        if result == 1:
+            print("勝ち")
+        elif result == 0:
+            print("引き分け")
+        else:
+            print("負け")
+
+
+class EnglishDisplay:
+    def show(self, result: int):
+        if result == 1:
+            print("win")
+        elif result == 0:
+            print("draw")
+        else:
+            print("lose")
 
 
 def main():
